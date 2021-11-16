5772599a2409f7a4efbd1c5d936793826756cdb5
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-16 15:18:23 +0330

    provide implementation to calculate interest

--- src/ir/dotin/model/data/LongTermDeposit.java
+++ src/ir/dotin/model/data/LongTermDeposit.java
@@ -1,5 +1,8 @@
 package ir.dotin.model.data;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * @author : Bahar Zolfaghari
  **/
@@ -13,5 +16,12 @@ public class LongTermDeposit extends Deposit {
 
     public static int getInterestRate() {
         return interestRate;
+    }
+
+    @Override
+    public BigDecimal calculateInterest() {
+        BigDecimal depositBalance = super.getDepositBalance();
+        int durationInDays = super.getDurationInDays();
+        return depositBalance.multiply(BigDecimal.valueOf(interestRate * durationInDays)).divide(BigDecimal.valueOf(36500), 4, RoundingMode.CEILING);
     }
 }
