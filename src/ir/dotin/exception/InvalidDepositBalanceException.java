03a40866762d3952e197945bd664fad90caf1f4b
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-16 13:34:07 +0330

    define invalid deposit balance exception

---
+++ src/ir/dotin/exception/InvalidDepositBalanceException.java
@@ -0,0 +1,11 @@
+package ir.dotin.exception;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class InvalidDepositBalanceException extends Exception {
+
+    public InvalidDepositBalanceException(String message) {
+        super(message);
+    }
+}
