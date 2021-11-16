202cebb8904a4e6052a146fa1e4f364ed73e660e
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-16 13:32:25 +0330

    define types of deposits with enum class

---
+++ src/ir/dotin/model/data/DepositType.java
@@ -0,0 +1,21 @@
+package ir.dotin.model.data;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public enum DepositType {
+    SHORT_TERM, LONG_TERM, QARZ;
+
+    public static DepositType getDepositType(String depositType) {
+        switch (depositType) {
+            case "ShortTerm":
+                return SHORT_TERM;
+            case "LongTerm":
+                return LONG_TERM;
+            case "Qarz":
+                return QARZ;
+            default:
+                return null;
+        }
+    }
+}
