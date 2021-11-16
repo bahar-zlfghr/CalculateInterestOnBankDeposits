5045adb7b5d0e904a979fcc4b2a29eae8501051e
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-16 15:53:46 +0330

    minor change

--- src/ir/dotin/Main.java
+++ src/ir/dotin/Main.java
@@ -9,6 +9,7 @@ import java.util.List;
  * @author : Bahar Zolfaghari
  **/
 public class Main {
+
     public static void main(String[] args) {
         List<Deposit> deposits = DepositRepository.getAllDeposits();
         deposits.sort(Deposit::compareTo);
