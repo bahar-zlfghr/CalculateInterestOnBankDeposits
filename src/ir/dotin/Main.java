ab683e124f552efb8e8e28daef84d27524e1678a
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-16 15:53:21 +0330

    start point of program

---
+++ src/ir/dotin/Main.java
@@ -0,0 +1,17 @@
+package ir.dotin;
+
+import ir.dotin.model.data.Deposit;
+import ir.dotin.model.repository.DepositRepository;
+
+import java.util.List;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class Main {
+    public static void main(String[] args) {
+        List<Deposit> deposits = DepositRepository.getAllDeposits();
+        deposits.sort(Deposit::compareTo);
+        DepositRepository.writeDepositsPayedInterestInfoInFile();
+    }
+}
