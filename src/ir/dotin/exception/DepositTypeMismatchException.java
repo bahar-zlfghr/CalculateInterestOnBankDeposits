a92b6b0886feca07c6a79cf4ee665db258428cb9
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-16 13:33:38 +0330

    define deposit type mismatch exception

---
+++ src/ir/dotin/exception/DepositTypeMismatchException.java
@@ -0,0 +1,11 @@
+package ir.dotin.exception;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class DepositTypeMismatchException extends Exception {
+
+    public DepositTypeMismatchException(String message) {
+        super(message);
+    }
+}
